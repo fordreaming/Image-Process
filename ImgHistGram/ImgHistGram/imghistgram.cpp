4d97ad142244ffd6e7bcd785366d118872400384
fordreaming <fordreaming1@yeah.net>
2018-08-23 14:30:09 +0800

    根据Qlabel,图像大小、窗口大小进行图片显示

--- ImgHistGram/ImgHistGram/imghistgram.cpp
+++ ImgHistGram/ImgHistGram/imghistgram.cpp
@@ -25,6 +25,7 @@ void ImgHistGram::ImgHistgramPro()
 	Histogram1D hist;
 	Mat histImg;
 	//通过文件对话框打开图像
+	QString StrWidth, StrHeigth;
 
 	QString fileName = QFileDialog::getOpenFileName(this, tr("Select Image"),
 		tr("."),
@@ -38,6 +39,7 @@ void ImgHistGram::ImgHistgramPro()
 	{
 		str = fileName.toStdString();
 		QImage* img = new QImage;
+		QImage* scaledImg = new QImage;//存储缩放后的图像
 		if(!( img->load(fileName) ) ) //加载图像
 		{
 			QMessageBox::information(this,
@@ -46,8 +48,36 @@ void ImgHistGram::ImgHistgramPro()
 			delete img;
 			return;
 		}
+		
+
+		ui.imgLabel->resize(img->width(), img->height());
+
+		ui.label_text->setText(QString("width: ") + StrWidth.setNum(img->width())
+			+ QString("\nheight: ") + StrHeigth.setNum(img->height()));
 		ui.imgLabel->setPixmap(QPixmap::fromImage(*img));
+
+		
+		//int imgWidth = img->width();
+		//int imgHeight = img->height();
+		////缩放后的图片大小
+		//int scaleImgWidth;
+		//int scaleImgHeight;
+
+		//ui.imgLabel->setGeometry(0,0,400,300);
+		//int Mul;            //记录图片与label大小的比例，用于缩放图片
+		//if(imgWidth/400 >= imgHeight/300)
+		//	Mul = imgWidth/400;
+		//else
+		//	Mul = imgHeight/300;
+		//scaleImgWidth = imgWidth/Mul;
+		//scaleImgHeight = imgHeight/Mul;
+		//*scaledImg = img->scaled(scaleImgWidth, scaleImgHeight , Qt::KeepAspectRatio);
+		//ui.imgLabel->setText(QString("width: ") + StrWidth.setNum(scaleImgWidth)
+		//	+ QString("\nheight: ") + StrHeigth.setNum(scaleImgHeight));
+		//ui.imgLabel->setPixmap(QPixmap::fromImage(*scaledImg));
 	}
+	//修改label的大小
+	
 	 
 
 	//Mat image = imread(str);
